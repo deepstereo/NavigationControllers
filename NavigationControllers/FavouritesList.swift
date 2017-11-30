40bf691f27feb3d21bbfc15c6dd132b3888e8817
Sergey Kozak <svkozak@icloud.com>
2017-11-29 19:48:03 -0500

    Added FavouritesList class

---
+++ NavigationControllers/FavouritesList.swift
@@ -0,0 +1,47 @@
+//
+//  FavouritesList.swift
+//  NavigationControllers
+//
+//  Created by Sergey Kozak on 29/11/2017.
+//  Copyright © 2017 Centennial. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class FavouritesList {
+    static let sharedFavouritesList = FavouritesList()
+    private(set) var favourites: [String]
+    
+    // Constructor
+    
+    init() {
+        let defaults = UserDefaults.standard
+        let storedFavourites = defaults.object(forKey: "favourites") as? [String]
+        favourites = storedFavourites != nil ? storedFavourites! : []
+    }
+    
+    func addFavourite (fontName: String) {
+        if !favourites.contains(fontName) {
+            favourites.append(fontName)
+            saveFavourites()
+        }
+    }
+    
+    // removes favourite
+    func removeFavourite (fontName: String) {
+        if let index = favourites.index(of: fontName) {
+            favourites.remove(at: index)
+            saveFavourites()
+        }
+    }
+    
+    // save favourites
+    private func saveFavourites () {
+        let defaults = UserDefaults.standard
+        defaults.set(favourites, forKey: "favourites")
+        defaults.synchronize()
+    }
+    
+    
+}
